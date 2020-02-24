e0fa2a36012e887e4cd9858afb25ce0d2d0187ba
Ibrahim Azam <ibrahim_azam@live.com>
2020-02-24 22:16:15 +0500

    heroku deploy milestone 1

---
+++ app/config/wooCommerce.js
@@ -0,0 +1,34 @@
+const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
+
+exports.test = async (req, res) => {
+    const WooCommerce = new WooCommerceRestApi({
+        url: 'https://bamushop.com/',
+        consumerKey: 'ck_753c6e532519c5643d8b69c0199f352c3fc5e423',
+        consumerSecret: 'cs_ef3f485c8879a02661c639d9543cd240e730045a',
+        version: 'wc/v3'
+    });
+
+    // List products
+    WooCommerce.get("products", {
+        per_page: 20, // 20 products per page
+    })
+        .then((response) => {
+            // Successful request
+            // console.log("Response Status:", response.status);
+            // console.log("Response Headers:", response.headers);
+            // console.log("Response Data:", response.data);
+            // console.log("Total of pages:", response.headers['x-wp-totalpages']);
+            // console.log("Total of items:", response.headers['x-wp-total']);
+            res.status(200).send(response.data)
+        })
+        .catch((error) => {
+            // Invalid request, for 4xx and 5xx statuses
+            // console.log("Response Status:", error.response.status);
+            // console.log("Response Headers:", error.response.headers);
+            // console.log("Response Data:", error.response.data);
+            res.status(400).json({error})
+        })
+        .finally(() => {
+            // Always executed.
+        });
+}
--- app/v1/api/api.js
+++ app/v1/api/api.js
@@ -4,10 +4,13 @@ const apiRoutes = express.Router()
 const UserController = require('../controllers/users');
 const { auth } = require('../middleware/auth');
 const { admin } = require('../middleware/admin');
+const WooCommerceController = require('../../config/wooCommerce');
 
 apiRoutes.get('/user/auth', auth, UserController.auth);
 apiRoutes.post('/user/register', UserController.register);
 apiRoutes.post('/user/login', UserController.login);
 apiRoutes.get('/user/logout', auth, UserController.logout);
+
+apiRoutes.get('/test', WooCommerceController.test)
 
 module.exports = apiRoutes
