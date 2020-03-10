a4dbd743b21f8244a0c74cf7ab7a840ede8590db
Ibrahim Azam <ibrahim_azam@live.com>
2020-03-10 12:04:03 +0500

    misc

--- app/v1/api/api.js
+++ app/v1/api/api.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const apiRoutes = express.Router()
-
-const UserController = require('../controllers/users');
-const OrderController = require('../controllers/orders');
-const CustomerController = require('../controllers/customers');
 const { auth } = require('../middleware/auth');
 const { admin } = require('../middleware/admin');
 const { wcPermission } = require('../middleware/wcPermission');
 const WooCommerceController = require('../../config/wooCommerce');
+const ProductController = require('../controllers/products');
+const UserController = require('../controllers/users');
+const OrderController = require('../controllers/orders');
+const CustomerController = require('../controllers/customers');
 
 
 /****
@@ -29,8 +29,12 @@ apiRoutes.get('/wc/order/all', auth, wcPermission, OrderController.getAll)
 apiRoutes.get('/wc/order/:orderId/refunds', auth, wcPermission, OrderController.refunds)
 apiRoutes.get('/wc/order/:orderId/notes', auth, wcPermission, OrderController.notes)
 apiRoutes.post('/wc/order/:orderId/note', auth, wcPermission, OrderController.addNote)
+apiRoutes.get('/wc/order/segment', auth, wcPermission, OrderController.segments)
 
 /**** Customers ****/
 apiRoutes.get('/wc/customer/all', auth, wcPermission, CustomerController.getAll)
+
+/**** Products ****/
+apiRoutes.get('/wc/product/all', auth, wcPermission, ProductController.getAll)
 
 module.exports = apiRoutes
--- app/v1/controllers/orders.js
+++ app/v1/controllers/orders.js
@@ -1,5 +1,7 @@
 const QueryString = require('querystring');
+const async = require('async');
 const { wcReturn } = require('../../lib/helpers')
+const constants = require('../../lib/constants');
 
 exports.getAll = async (req, res) => {
     try {
@@ -36,6 +38,88 @@ exports.addNote = async (req, res) => {
         let response = await req.WooCommerce.post(`orders/${req.params.orderId}/notes`, req.body)
         return res.json(wcReturn(response))
     } catch (error) {
+        res.status(400).json(error.response.data)
+    }
+}
+
+exports.segments = async (req, res) => {
+    try {
+
+
+        async.parallel([
+            async () => {       //  successfull orders
+                let response = await req.WooCommerce.get(`orders?per_page=100&status=completed`)
+                response = wcReturn(response)
+                if (response.pages > 1) {
+                    for (let x = 2; x <= response.pages; x++) {
+                        let newResponse = await req.WooCommerce.get(`orders?page=${x}&per_page=100&status=completed`)
+                        newResponse = wcReturn(newResponse)
+                        response.data = response.data.concat(newResponse.data)
+                    }
+                }
+                let successfull = {
+                    name: 'Successfull Orders',
+                    orders: response.data.length,
+                    grossSales: 0,
+                    totalItems: 0,
+                    AOV: 0,
+                    AOI: 0
+                }
+                for (let resp of response.data) {
+                    successfull.grossSales = successfull.grossSales + +resp.total
+                    successfull.totalItems = successfull.totalItems + +resp.line_items.length
+                }
+                successfull.grossSales = Math.round(((successfull.grossSales) + Number.EPSILON) * 100) / 100
+                successfull.AOV = Math.round(((successfull.grossSales / successfull.orders) + Number.EPSILON) * 100) / 100;
+                successfull.AOI = Math.round(((successfull.totalItems / successfull.orders) + Number.EPSILON) * 100) / 100;
+                return successfull
+            },
+            async () => {   //  failed orders
+                let response = await req.WooCommerce.get(`orders?per_page=100&status=failed`)
+                response = wcReturn(response)
+                if (response.pages > 1) {
+                    for (let x = 2; x <= response.pages; x++) {
+                        let newResponse = await req.WooCommerce.get(`orders?page=${x}&per_page=100&status=failed`)
+                        newResponse = wcReturn(newResponse)
+                        response.data = response.data.concat(newResponse.data)
+                    }
+                }
+                let failed = {
+                    name: 'Failed Orders',
+                    orders: response.data.length,
+                    grossSales: 0,
+                    totalItems: 0,
+                    AOV: 0,
+                    AOI: 0
+                }
+                for (let resp of response.data) {
+                    failed.grossSales = failed.grossSales + +resp.total
+                    failed.totalItems = failed.totalItems + +resp.line_items.length
+                }
+                failed.grossSales = Math.round(((failed.grossSales) + Number.EPSILON) * 100) / 100
+                failed.AOV = Math.round(((failed.grossSales / failed.orders) + Number.EPSILON) * 100) / 100;
+                failed.AOI = Math.round(((failed.totalItems / failed.orders) + Number.EPSILON) * 100) / 100;
+                return failed
+            },
+            // async function (cb) {
+
+            // },
+            // async function (cb) {
+
+            // },
+            // async function (cb) {
+
+            // },
+            // async function (cb) {
+
+            // }
+        ],
+            (err, result) => {
+                if (err) throw err
+                return res.send(result)
+            })
+    } catch (error) {
+        console.error(error)
         res.status(400).json(error.response.data)
     }
 }
